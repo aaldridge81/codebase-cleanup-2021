386978c3e7601d813b5b43b945999858d650c6a9
Alyssa Aldridge <ana81@georgetown.edu>
2021-03-24 21:17:54 -0400

    WIP - started to reduce complexity

--- app/game.py
+++ app/game.py
@@ -23,23 +23,54 @@ print("COMPUTER CHOICE:", c)
 # DETERMINATION OF WINNER
 #
 
-if u == "rock" and c == "rock":
-    print("It's a tie!")
-elif u == "rock" and c == "paper":
-    print("The computer wins")
-elif u == "rock" and c == "scissors":
-    print("The user wins")
+# if u == "rock" and c == "rock":
+#     print("It's a tie!")
+# elif u == "rock" and c == "paper":
+#     print("The computer wins")
+# elif u == "rock" and c == "scissors":
+#     print("The user wins")
+# 
+# elif u == "paper" and c == "rock":
+#     print("The computer wins")
+# elif u == "paper" and c == "paper":
+#     print("It's a tie!")
+# elif u == "paper" and c == "scissors":
+#     print("The user wins")
+# 
+# elif u == "scissors" and c == "rock":
+#     print("The computer wins")
+# elif u == "scissors" and c == "paper":
+#     print("The user wins")
+# elif u == "scissors" and c == "scissors":
+#     print("It's a tie!")
 
-elif u == "paper" and c == "rock":
-    print("The computer wins")
-elif u == "paper" and c == "paper":
-    print("It's a tie!")
-elif u == "paper" and c == "scissors":
-    print("The user wins")
 
-elif u == "scissors" and c == "rock":
-    print("The computer wins")
-elif u == "scissors" and c == "paper":
-    print("The user wins")
-elif u == "scissors" and c == "scissors":
-    print("It's a tie!")
+if u == "rock":
+    if c == "rock":
+        print("_____")
+    elif c == "scissors":
+        print("_____")
+    elif c == "scissors":
+        print("_____")
+elif u == "paper":
+    if c == "rock":
+        print("_____")
+    elif c == "scissors":
+        print("_____")
+    elif c == "scissors":
+        print("_____")
+elif u == "scissors":
+    if c == "rock":
+        print("_____")
+    elif c == "scissors":
+        print("_____")
+    elif c == "scissors":
+        print("_____")
+
+## use dictionary approach
+
+# winners = {
+#     u == "rock"
+# 
+# }
+
