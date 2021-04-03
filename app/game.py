e179996b6e7e12c78c1d8d7af61b667efea560d9
aaldridge81 <77745907+aaldridge81@users.noreply.github.com>
2021-04-02 20:52:31 -0400

    Testing our game (#3)

--- app/game.py
+++ app/game.py
@@ -1,77 +1,70 @@
 
 from random import choice
 
-#
-# USER SELECTION
-#
 
 VALID_OPTIONS = ["rock", "paper", "scissors"]
-u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
-print("USER CHOICE:", u)
-if u not in VALID_OPTIONS:
-    print("OOPS, TRY AGAIN")
-    exit()
-
-#
-# COMPUTER SELECTION
-#
-
-c = choice([VALID_OPTIONS])
-print("COMPUTER CHOICE:", c)
-
-#
-# DETERMINATION OF WINNER
-#
-
-# if u == "rock" and c == "rock":
-#     print("It's a tie!")
-# elif u == "rock" and c == "paper":
-#     print("The computer wins")
-# elif u == "rock" and c == "scissors":
-#     print("The user wins")
-# 
-# elif u == "paper" and c == "rock":
-#     print("The computer wins")
-# elif u == "paper" and c == "paper":
-#     print("It's a tie!")
-# elif u == "paper" and c == "scissors":
-#     print("The user wins")
-# 
-# elif u == "scissors" and c == "rock":
-#     print("The computer wins")
-# elif u == "scissors" and c == "paper":
-#     print("The user wins")
-# elif u == "scissors" and c == "scissors":
-#     print("It's a tie!")
 
 
-if u == "rock":
-    if c == "rock":
-        print("_____")
-    elif c == "scissors":
-        print("_____")
-    elif c == "scissors":
-        print("_____")
-elif u == "paper":
-    if c == "rock":
-        print("_____")
-    elif c == "scissors":
-        print("_____")
-    elif c == "scissors":
-        print("_____")
-elif u == "scissors":
-    if c == "rock":
-        print("_____")
-    elif c == "scissors":
-        print("_____")
-    elif c == "scissors":
-        print("_____")
+def determine_winner(choice1, choice2):
+     """
+     Params:
+         choice1 and choice2 are both string : one of "rock", "paper", or "scissors"
+     """
+     winners = {
+         "rock":{
+             "rock": None, 
+             "paper": "paper",
+             "scissors": "rock",
+         },
+         "paper":{
+             "rock": "paper",
+             "paper": None, 
+             "scissors": "scissors",
+         },
+         "scissors":{
+             "rock": "rock",
+             "paper": "scissors",
+             "scissors": None, 
+         },
+     }
+     winning_choice = winners[choice1][choice2]
+     return winning_choice
 
-## use dictionary approach
 
-# winners = {
-#     u == "rock"
-# 
-# }
+if __name__ == "__main__":
+    pass
 
-## integrate with code climate
+
+    #
+    # USER SELECTION
+    #
+
+    
+    u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
+    print("USER CHOICE:", u)
+    if u not in VALID_OPTIONS:
+        print("OOPS, TRY AGAIN")
+        exit()
+
+    #
+    # COMPUTER SELECTION
+    #
+
+    c = choice(VALID_OPTIONS)
+    print("COMPUTER CHOICE:", c)
+
+    #
+    # DETERMINATION OF WINNER
+    #
+    
+
+    winner = determine_winner(u, c)
+    
+
+    if winner == u:
+        print("YOU WON!")
+    elif winner == c:
+        print("COMPUTER WON")
+    elif winner == None:
+        print("TIE")
+    ## integrate with code climate
