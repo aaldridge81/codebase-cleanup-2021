11125e8f1cda0eadfe5f8aa2c9dd40dd23909f1e
Alyssa Aldridge <ana81@georgetown.edu>
2021-04-03 14:36:19 -0400

    Testing Shopping

--- test/shopping_test.py
+++ test/shopping_test.py
@@ -16,22 +16,22 @@ def test_format_usd():
 
 
 @pytest.fixture(scope="module")
- def mock_products():
+def mock_products():
      mock_products_filepath = os.path.join(os.path.dirname(__file__), "mock_data", "mock_products.csv")
      mock_products_df = read_csv(mock_products_filepath)
      return mock_products_df.to_dict("records")
 
 
- def test_lookups(mock_products):
+def test_lookups(mock_products):
      # with valid product id, returns the product info:
-     valid_result = lookup_product("8", mock_products)
-     assert valid_result == {
-         'aisle': 'Aisle C',
-         'department': 'snacks',
-         'id': 8,
-         'name': 'Product 8',
-         'price': 10.0
-     }
-     # with invalid product id, returns None:
-     invalid_result = lookup_product("88888888", mock_products)
-     assert invalid_result == None
+    valid_result = lookup_product("8", mock_products)
+    assert valid_result == {
+        'aisle': 'Aisle C',
+        'department': 'snacks',
+        'id': 8,
+        'name': 'Product 8',
+        'price': 10.0
+    }
+    # with invalid product id, returns None:
+    invalid_result = lookup_product("88888888", mock_products)
+    assert invalid_result == None
